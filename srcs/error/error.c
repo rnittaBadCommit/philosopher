6cd0b1f9b572a1bd6915bb52391f53a11656218e
rnitta <ryushironitta@yahoo.co.jp>
2022-06-15 18:59:27 +0900

    add error output

--- srcs/error/error.c
+++ srcs/error/error.c
@@ -3,10 +3,18 @@
 int ft_error(t_all *all)
 {
     printf("Error:\n");
-    if (all->err == INVALID_ARG_NUM)
+    if (all->err == MALLOC_FAIL)
+        printf("malloc failed\n");
+    else if (all->err == INPUT_NULL_POINTER)
+        printf("pass NULL as argument\n");
+    else if (all->err == INVALID_ARG_NUM)
         printf("invalid number of args\n");
     else if (all->err == INVALID_ARG_VALUE)
         printf("invalid value of args\n");
+    else if (all->err == OVER_FLOW)
+        printf("over flow happened\n");
+    else if (all->err == NO_DIGITS)
+        printf("atoi convert to 0 from no digits\n");
     
     ft_finalize(all);
     return (-1);
