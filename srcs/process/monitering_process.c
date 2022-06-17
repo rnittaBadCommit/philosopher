1218fa67677b6fb4c98c9a855ed6e7a320aaab6f
rnitta <ryushironitta@yahoo.co.jp>
2022-06-17 17:49:20 +0900

    死亡を検知

--- srcs/process/monitering_process.c
+++ srcs/process/monitering_process.c
@@ -5,7 +5,23 @@ int	_is_all_survive(s_philosopher *philosopher_arry)
 
 }
 
-void	moniter_philosophers()
+void	moniter_philosophers(void *p)
 {
-	
+	t_all			*all;
+	t_philosopher	*philosopher_arry;
+	int				i;
+	int				time;
+
+	all = p;
+	while (1)
+	{
+		time = ft_get_time_sec();
+		i = 0;
+		while (i < all->philosopher_data.num_philosophers)
+		{
+			if (philosopher_arry[i].time_die <= time - philosopher_arry[i].time_last_eat)
+				
+
+		}
+	}
 }
